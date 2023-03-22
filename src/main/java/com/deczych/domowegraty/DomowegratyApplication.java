f4bd3d93c46c56bc5493371d0468c4c675f76e4b
krystiandec <decik1991@gmail.com>
2023-03-22 21:34:37 +0100

    1commit

---
+++ src/main/java/com/deczych/domowegraty/DomowegratyApplication.java
@@ -0,0 +1,13 @@
+package com.deczych.domowegraty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomowegratyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomowegratyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deczych/domowegraty/DomowegratyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deczych.domowegraty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomowegratyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
