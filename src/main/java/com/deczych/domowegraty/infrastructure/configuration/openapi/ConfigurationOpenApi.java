81a5018908408b8b20f38096cf6f5bfbc4c80a91
krystiandec <decik1991@gmail.com>
2023-04-03 17:27:52 +0200

    Dodanie Klasy konfiguracyjnej dla OpenAPI. i personalizacja swaggera w niej.

---
+++ src/main/java/com/deczych/domowegraty/infrastructure/configuration/openapi/ConfigurationOpenApi.java
@@ -0,0 +1,28 @@
+package com.deczych.domowegraty.infrastructure.configuration.openapi;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigurationOpenApi {
+
+    @Bean
+    public OpenAPI gratyOpenApi() {
+        return new OpenAPI()
+                .info(new Info().title("PrzytulnyGrajdołek")
+                        .description("TestOpisu")
+                        .version("1.0.0")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("SpringShop Wiki Documentation")
+                        .url("https://springshop.wiki.github.org/docs")
+                );
+
+    }
+
+
+}
