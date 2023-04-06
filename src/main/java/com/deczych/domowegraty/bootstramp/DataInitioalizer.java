449cde9672feed2d8e2caff308b5186302ef6922
krystiandec <decik1991@gmail.com>
2023-04-06 15:27:52 +0200

    Initialize more items

--- src/main/java/com/deczych/domowegraty/bootstramp/DataInitioalizer.java
+++ src/main/java/com/deczych/domowegraty/bootstramp/DataInitioalizer.java
@@ -24,11 +24,26 @@ public class DataInitioalizer implements CommandLineRunner {
         ItemDetailData item1 = ItemDetailData.builder()
                 .price(new BigDecimal(123.24))
                 .creationDate(Instant.now())
-                .weight(new BigDecimal(21))
+                .weight(new BigDecimal(2.1))
                 .productCode(598741231564l)
                 .build();
-        System.out.println("Data item1:\n" + item1.toString());
+        System.out.println("Data item2:\n" + item1.toString());
         repository.save(item1);
+        ItemDetailData item2 = ItemDetailData.builder()
+                .price(new BigDecimal(223.24))
+                .creationDate(Instant.now())
+                .weight(new BigDecimal(1.05))
+                .productCode(5987412315642l)
+                .build();
+        System.out.println("Data item3:\n" + item1.toString());
+        repository.save(item2);
+        ItemDetailData item3 = ItemDetailData.builder()
+                .price(new BigDecimal(323.24))
+                .creationDate(Instant.now())
+                .weight(new BigDecimal(0.85))
+                .productCode(5987412315643l)
+                .build();
+        System.out.println("Data item1:\n" + item1.toString());
 
 //        ItemDetailData itemSaved= repository.save(item1);
 //        System.out.println("Data itemSaved:\n"+itemSaved.toString());
